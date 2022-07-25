97723ebc536b6dca15183cc4557285f30f1af999
ayoline <ayo@outlook.com.br>
2022-07-25 15:05:31 -0300

    add Dashboard component

--- src/pages/Dashboard/Extract/Extract.tsx
+++ src/pages/Dashboard/Extract/Extract.tsx
@@ -1,3 +1,10 @@
+import Dashboard from '../../../components/atoms/Dashboard';
+import './styles.css';
+
 export const Extract = () => {
-  return <h1>Extract</h1>;
+  return (
+    <div className="extract-div container">
+      <Dashboard>teste</Dashboard>
+    </div>
+  );
 };
