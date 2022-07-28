00a19dc1e71cab4ad52af62f6d8ef136d899d537
ayoline <ayo@outlook.com.br>
2022-07-28 19:03:16 -0300

    add dashboard to all pages

--- src/pages/Dashboard/Withdraw/Withdraw.tsx
+++ src/pages/Dashboard/Withdraw/Withdraw.tsx
@@ -1,3 +1,10 @@
+import Dashboard from '../../../components/atoms/Dashboard';
+
 export const Withdraw = () => {
-  return <h1>Withdraw</h1>;
+  return (
+    <div className="bg-body-light-200 w-sm h-sm flex flex-col justify-start items-center mx-auto my-6 min-h-min">
+      <Dashboard>Fulano</Dashboard>
+      <p>WithDraw</p>
+    </div>
+  );
 };
