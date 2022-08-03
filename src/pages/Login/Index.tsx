8cccf7c2811ea9b24337c8f18af01701d159119d
Dudeful <rellumnyar@gmail.com>
2022-08-03 00:29:36 -0300

    add request to get data from join user/account

--- src/pages/Login/Index.tsx
+++ src/pages/Login/Index.tsx
@@ -1,14 +1,21 @@
-import { useContext } from 'react';
+import { useContext, createContext } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Input } from '../../components/atoms';
 import { UserContext } from '../../providers/User';
 
 export const Login = () => {
 	const handleLogin = () => {
-		console.log('entrou');
+		const cpf = (document.getElementById('login_cpf') as HTMLInputElement)
+			.value;
+
+		fetch(`http://gcp.dudeful.com:5000/users?cpf=${cpf}`)
+			.then((res) => res.json())
+			.then((data) => console.log(data))
+			.catch((error) => console.error(error));
 	};
-	const user = useContext(UserContext)
-	console.log(user)
+
+	const user = useContext(UserContext);
+	console.log(user);
 	return (
 		<div className="bg-body-light-200 dark:bg-body-dark w-sm h-sm flex flex-col justify-start items-center mx-auto min-h-min my-[20px]">
 			<img className="w-32 h-fit mt-20" src="./src/assets/logo.svg" alt="" />
@@ -16,8 +23,12 @@ export const Login = () => {
 				Login
 			</p>
 			<form className="w-64 flex flex-col items-center gap-5 mb-3" action="">
-				<Input placeholder="Digite seu CPF" type="text" />
-				<Input placeholder="Digite sua senha" type="password" />
+				<Input id="login_cpf" placeholder="Digite seu CPF" type="text" />
+				<Input
+					id="login_password"
+					placeholder="Digite sua senha"
+					type="password"
+				/>
 				<Button type="button" onClick={handleLogin}>
 					Entrar
 				</Button>
--- src/providers/User/index.tsx
+++ src/providers/User/index.tsx
@@ -1,16 +1,12 @@
-import React, {createContext, PropsWithChildren} from "react"
+import React, { createContext, PropsWithChildren } from 'react';
 
-export const UserContext = createContext({})
+export const UserContext = createContext({});
 
-export const UserProvider:React.FC<PropsWithChildren> = ({children}) => {
-    const user = {
-        account: "account-test",
-        agency: "agency-test"
-    }
-    
-    return(
-        <UserContext.Provider value={user}>
-            {children}
-        </UserContext.Provider>
-    )
-}
+export const UserProvider: React.FC<PropsWithChildren> = ({ children }) => {
+	const user = {
+		account: 'account-test',
+		agency: 'agency-test',
+	};
+
+	return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
+};
