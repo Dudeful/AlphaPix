2cf4665e383df9c31680edfb5665a50e38d85180
ayoline <ayo@outlook.com.br>
2022-08-03 12:49:06 -0300

    remove all desnecessary css files

--- src/components/atoms/Title/index.tsx
+++ src/components/atoms/Title/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import './styles.css';
 
 interface ITitleProps {
-  classNames: string;
-  value?: string;
+	classNames: string;
+	value?: string;
 }
 
 const Title: React.FC<ITitleProps> = ({ value, classNames }) => {
-  return <h3 className={classNames}>{value}</h3>;
+	return <h3 className={classNames}>{value}</h3>;
 };
 
 export default Title;
--- src/components/molecules/AccountData/index.tsx
+++ src/components/molecules/AccountData/index.tsx
@@ -1,48 +1,51 @@
 import React from 'react';
 import { Input } from '../../atoms';
 import { Title } from '../../atoms';
-import './styles.css';
 
 interface IAccountDataProps {
-  title: string;
-  value_1?: string;
-  value_2?: string;
-  readOnly?: boolean;
-  classNames?: string;
+	title: string;
+	value_1?: string;
+	value_2?: string;
+	readOnly?: boolean;
+	classNames?: string;
 }
 
 const AccountData: React.FC<IAccountDataProps> = ({
-  title,
-  value_1,
-  value_2,
-  readOnly,
-  classNames,
+	title,
+	value_1,
+	value_2,
+	readOnly,
+	classNames,
 }) => {
-  return (
-    <>
-      <Title classNames="" value={title} />
-      <div className="flex justify-between">
-        <div className="w-[45%] mb-5">
-          <Input
-            classNames={'block h-[45px] p-3 text-[18px] ' + classNames}
-            type={'text'}
-            value={value_1}
-            readOnly={readOnly}
-          />
-          <label className="text-slate-500 mt-1 ml-1 block text-sm text-left">Agência</label>
-        </div>
-        <div className="w-[45%]">
-          <Input
-            classNames={'block h-[45px] p-3 text-[18px] ' + classNames}
-            type={'text'}
-            value={value_2}
-            readOnly={readOnly}
-          />
-          <label className="text-slate-500 mt-1 ml-1 block text-sm text-left">Conta</label>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Title classNames="" value={title} />
+			<div className="flex justify-between">
+				<div className="w-[45%] mb-5">
+					<Input
+						classNames={'block h-[45px] p-3 text-[18px] ' + classNames}
+						type={'text'}
+						value={value_1}
+						readOnly={readOnly}
+					/>
+					<label className="text-slate-500 mt-1 ml-1 block text-sm text-left">
+						Agência
+					</label>
+				</div>
+				<div className="w-[45%]">
+					<Input
+						classNames={'block h-[45px] p-3 text-[18px] ' + classNames}
+						type={'text'}
+						value={value_2}
+						readOnly={readOnly}
+					/>
+					<label className="text-slate-500 mt-1 ml-1 block text-sm text-left">
+						Conta
+					</label>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default AccountData;
