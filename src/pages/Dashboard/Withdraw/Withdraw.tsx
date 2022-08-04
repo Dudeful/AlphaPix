a0203d5e061e5a30da669b0f63095a7c5d0baf1a
Dudeful <rellumnyar@gmail.com>
2022-08-04 13:14:35 -0300

    fix more things than we should in a single commit :(

--- src/pages/Dashboard/Withdraw/Withdraw.tsx
+++ src/pages/Dashboard/Withdraw/Withdraw.tsx
@@ -4,19 +4,52 @@ import Dashboard from '../../../components/atoms/Dashboard';
 import Input from '../../../components/atoms/Input';
 import Modal from '../../../components/atoms/Modal/Index';
 import UserContext from '../../../providers/User';
+import { getData } from '../../../service/getdata';
 
 export const Withdraw = () => {
 	const [isActive, setIsActive] = useState(false);
-	const [userState, setUserState] = useContext(UserContext)
+	const [userState, setUserState] = useContext(UserContext);
+	const [amount, setAmount] = useState('');
+	const [password, setPassword] = useState('');
+
+	const data = {
+		origin: {
+			branch: userState[0].branch,
+			account: userState[0].account_number,
+		},
+		amount,
+		password,
+	};
+
+	const options = {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({ data }),
+	};
+
 	const handleWithdraw = () => {
-		console.log('entrou');
+		console.log({ data });
+		fetch('http://gcp.dudeful.com:5000/register-withdraw', options)
+			.then((res) => res.json())
+			.then((res) => {
+				console.log(res);
+				getData(userState[0].cpf).then((data) => {
+					setUserState(data);
+				});
+
+				setAmount('');
+				setPassword('');
+				handleCloseModal();
+			})
+			.catch((error) => console.error(error));
+	};
+	const handleCloseModal = () => {
 		setIsActive((current) => !current);
 	};
-	const handleCloseModal = () => {setIsActive((current) => !current)}
 
 	return (
 		<div className="bg-body-light-200 dark:bg-body-dark w-sm h-sm flex flex-col justify-start items-center mx-auto min-h-min my-[20px]">
-			<Dashboard>Fulano</Dashboard>
+			<Dashboard>{userState[0].name}</Dashboard>
 			<div className="w-[280px] h-[384] py-[12px] px-[15px] rounded-[8px] bg-white flex flex-col dark:border dark:border-solid dark:border-btn-secondary-base dark:bg-body-dark">
 				<div className="flex flex-row gap-[10px]">
 					<img src="../src/assets/withdraw-gold.svg" alt="" />
@@ -47,21 +80,38 @@ export const Withdraw = () => {
 				</div>
 
 				<div className="mt-[15px]">
-					<Input placeholder="Valor" type="number"></Input>
 					<Input
+						value={amount}
+						inputHandler={(e: React.FormEvent<HTMLInputElement>) =>
+							setAmount(e.currentTarget.value)
+						}
+						id="deposit_amount"
+						placeholder="Valor"
+						type="number"
+					></Input>
+					<Input
+						value={password}
+						id="deposit_password"
 						placeholder="Senha"
 						type="password"
 						className="mt-[20px]"
+						inputHandler={(e: React.FormEvent<HTMLInputElement>) =>
+							setPassword(e.currentTarget.value)
+						}
 					></Input>
 					<Button
+						onClick={handleCloseModal}
 						className="mt-[15px] bg-btn-primary-base"
-						onClick={handleWithdraw}
 					>
 						Sacar
 					</Button>
 				</div>
 			</div>
-			<Modal showModal={isActive} handleClose={handleCloseModal}></Modal>
+			<Modal
+				showModal={isActive}
+				handleClose={handleCloseModal}
+				handleConfirm={handleWithdraw}
+			></Modal>
 		</div>
 	);
 };
